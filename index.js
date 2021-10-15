24671823d74c98d97f52dbc04ae4787c1fc020b5
laris <larisara@gmail.com>
2021-10-16 01:48:52 +0700

    git change response structure

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ const WEB3_URL = 'https://bsc-dataseed1.ninicoin.io'
 const web3 = new Web3(WEB3_URL)
 const tokens = require('./consts/token')
 const { NETWORKS, POOLS } = require('./consts/index')
-const { getInfo } = require('./utils')
+const { getInfo, getTVL } = require('./utils')
 
 !(async function () {
-    const harmony = await getInfo(POOLS[NETWORKS.HARMONY_ONE.chainId])
-    const fantom = await getInfo(POOLS[NETWORKS.FANTOM.chainId])
+  const harmonyPools = await getInfo(POOLS[NETWORKS.HARMONY_ONE.chainId])
+  const fantomPools = await getInfo(POOLS[NETWORKS.FANTOM.chainId])
+  const harmonyTVL = await getTVL(POOLS[NETWORKS.HARMONY_ONE.chainId])
+  const fantomTVL = await getTVL(POOLS[NETWORKS.FANTOM.chainId])
 
-    let result = {}
-    Object.assign(result, { [NETWORKS.HARMONY_ONE.chainId]: harmony })
-    Object.assign(result, { [NETWORKS.FANTOM.chainId]: fantom })
-    console.log(result)
-    let fs = require('fs')
-    fs.writeFileSync('/root/twinprime-api/www-data/static/multi-chain', JSON.stringify(result))
+  let result = {}
+  Object.assign(result, { [NETWORKS.HARMONY_ONE.chainId]: harmonyPools })
+  Object.assign(result, { [NETWORKS.FANTOM.chainId]: fantomPools })
+  result['tvl'] = {
+    [NETWORKS.HARMONY_ONE.chainId]: harmonyTVL,
+    [NETWORKS.FANTOM.chainId]: fantomTVL,
+  }
+  console.log(result)
+  let fs = require('fs')
+  fs.writeFileSync('/root/twinprime-api/www-data/static/multi-chain', JSON.stringify(result))
 })()
--- utils/index.js
+++ utils/index.js
@@ -5,54 +5,61 @@ const SWAP_ABI = require('../consts/abis/Swap.abi.json')
 const ethers = require('ethers')
 
 async function getTotalSupply(address, provider) {
-    const pair = new ethers.Contract(address, PAIR_ABI, provider)
-    const _totalSupply = await pair.totalSupply()
-    return _totalSupply
+  const pair = new ethers.Contract(address, PAIR_ABI, provider)
+  const _totalSupply = await pair.totalSupply()
+  return _totalSupply
 }
 
 async function getVirtualPrice(address, provider) {
-    const swap = new ethers.Contract(address, SWAP_ABI, provider)
-    const _virtualPrice = await swap.getVirtualPrice()
-    return _virtualPrice
+  const swap = new ethers.Contract(address, SWAP_ABI, provider)
+  const _virtualPrice = await swap.getVirtualPrice()
+  return _virtualPrice
 }
 async function getTokenBalance(data, provider) {
-    const swap = new ethers.Contract(data.swapAddress, SWAP_ABI, provider)
-    let result = []
-    for (const [k, v] of Object.entries(data.tokensInPool)) {
-        const _tokenBalance = await swap.getTokenBalance(k)
-        result.push({ ...v, tokenBalance: _tokenBalance, ts: _tokenBalance.toString() })
-    }
-    const map = result.map((x) => ethers.utils.formatUnits(x.tokenBalance, x.decimals))
-    let poolTvl = ethers.constants.Zero
-    for (const v of map) {
-        poolTvl = poolTvl.add(ethers.utils.parseEther(v))
-    }
-    return poolTvl
+  const swap = new ethers.Contract(data.swapAddress, SWAP_ABI, provider)
+  let result = []
+  for (const [k, v] of Object.entries(data.tokensInPool)) {
+    const _tokenBalance = await swap.getTokenBalance(k)
+    result.push({ ...v, tokenBalance: _tokenBalance, ts: _tokenBalance.toString() })
+  }
+  const map = result.map((x) => ethers.utils.formatUnits(x.tokenBalance, x.decimals))
+  let poolTvl = ethers.constants.Zero
+  for (const v of map) {
+    poolTvl = poolTvl.add(ethers.utils.parseEther(v))
+  }
+  return poolTvl
 }
+
+async function getTVL(data) {
+  const provider = new ethers.providers.JsonRpcProvider(data.rpcUrl)
+  let tvl = ethers.constants.Zero
+  for (const [k, v] of Object.entries(data.pools)) {
+    const poolTvl = await getTokenBalance(v, provider)
+    tvl = tvl.add(poolTvl)
+  }
+  return ethers.utils.formatEther(tvl)
+}
+
 async function getInfo(data) {
-    const provider = new ethers.providers.JsonRpcProvider(data.rpcUrl)
-    let result = []
-    let tvl = ethers.constants.Zero
-    for (const [k, v] of Object.entries(data.pools)) {
-        const _totalSupply = await getTotalSupply(v.address, provider)
-        const _virtualPrice = await getVirtualPrice(v.swapAddress, provider)
-        const poolTvl = await getTokenBalance(v, provider)
-        tvl = tvl.add(poolTvl)
-        result.push({
-            ...v,
-            _totalSupply: _totalSupply.toString(),
-            _virtualPrice: _virtualPrice.toString(),
-            totalSupply: _totalSupply
-                .div(ethers.constants.WeiPerEther)
-                .mul(_virtualPrice)
-                .div(ethers.constants.WeiPerEther)
-                .toString(),
-        })
-    }
-    result['tvl'] = ethers.utils.formatEther(tvl)
-    return result
+  const provider = new ethers.providers.JsonRpcProvider(data.rpcUrl)
+  let result = []
+  for (const [k, v] of Object.entries(data.pools)) {
+    const _totalSupply = await getTotalSupply(v.address, provider)
+    const _virtualPrice = await getVirtualPrice(v.swapAddress, provider)
+    result.push({
+      ...v,
+      _totalSupply: _totalSupply.toString(),
+      _virtualPrice: _virtualPrice.toString(),
+      totalSupply: _totalSupply
+        .div(ethers.constants.WeiPerEther)
+        .mul(_virtualPrice)
+        .div(ethers.constants.WeiPerEther)
+        .toString(),
+    })
+  }
+  return result
 }
 
 module.exports = {
-    getInfo,
+  getInfo, getTVL
 }
